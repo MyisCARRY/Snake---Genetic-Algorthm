5b53372f73995d2975da190e92b9f74ace4bb6ca
MyisCARRY <tataauto@interia.pl>
2019-07-04 18:40:51 +0200

    Classes that supports database

---
+++ DBConnection.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace SnakeGA
+{
+    public class DBConnection
+    {
+        private DBConnection()
+        {
+        }
+
+        private MySqlConnection connection = null;
+        public MySqlConnection Connection
+        {
+            get { return connection; }
+        }
+
+        private static DBConnection _instance = null;
+        public static DBConnection Instance()
+        {
+            if (_instance == null)
+                _instance = new DBConnection();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            if (Connection == null)
+            {
+                try
+                {
+                    string connstring = string.Format($"Server={DBCredentials.Server}; database={DBCredentials.DBName}; UID={DBCredentials.UID}; password={DBCredentials.Password}");
+                    connection = new MySqlConnection(connstring);
+                    connection.Open();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e);
+                    return false;
+                }
+                
+            }
+
+            return true;
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+
+        public static void UpdateInsert(string query)
+        {
+            var dbCon = Instance();
+
+            if (dbCon.IsConnect())
+            {
+                var cmd = new MySqlCommand();
+                cmd.Connection = dbCon.Connection;
+                cmd.CommandText = query;
+                cmd.ExecuteNonQuery();
+                //dbCon.Close();
+            }
+        }
+    }
+}
---
+++ DBHelper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace SnakeGA
+{
+    class DBHelper
+    {
+
+        public static List<InitList> GetInitLists()
+        {
+            var dbConnection = DBConnection.Instance();
+            var result = new List<InitList>();
+            if(dbConnection.IsConnect())
+            {
+                string query = "select * from population;";
+
+                var command = new MySqlCommand(query, dbConnection.Connection);
+                var reader = command.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    const int DigitsToRound = 3;
+                    InitList initList = new InitList();
+                    initList.Id = reader.GetInt32("Id");
+                    initList.Name = reader.GetString("Name");
+                    initList.HiddenLayers = reader.GetInt32("HiddenLayers");
+                    initList.HiddenNodes = reader.GetInt32("HiddenNodes");
+                    initList.Size = reader.GetInt32("Size");
+                    initList.MutationRate = Math.Round(reader.GetDouble("MutationRate"), DigitsToRound);
+                    initList.TopPopulation = Math.Round(reader.GetDouble("Top"), DigitsToRound);
+                    initList.MovesAtStart = reader.GetInt32("MovesAtStart");
+                    initList.MovesForFood = reader.GetInt32("MovesForFood");
+                    initList.BestScore = reader.GetInt32("BestScore");
+                    result.Add(initList);
+                }
+
+                reader.Close();
+                //dbConnection.Close();
+
+                
+            }
+
+            return result;
+        }
+
+        public static Tuple<int[],int[], int[]> GetGraphData(int id)
+        {
+            int[] avg = new int[0];
+            int[] min = new int[0];
+            int[] max = new int[0];
+            var dbConnection = DBConnection.Instance();
+            if (dbConnection.IsConnect())
+            {
+                string query = $"select avg(Score),max(Score),min(Score), generation from snakes where IdPopulation = {id} group by generation;";
+                var command = new MySqlCommand(query, dbConnection.Connection);
+                var reader = command.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                int numberOfResults = dt.Rows.Count;
+                avg = new int[numberOfResults];
+                min = new int[numberOfResults];
+                max = new int[numberOfResults];
+                for (int i = 0; i < numberOfResults; i++)
+                {
+                    avg[i] = (int)double.Parse(dt.Rows[i][0].ToString());
+                    max[i] = (int)double.Parse(dt.Rows[i][1].ToString());
+                    min[i] = (int)double.Parse(dt.Rows[i][2].ToString());
+                }
+
+                reader.Close();
+                //dbConnection.Close();
+
+
+            }
+
+            return new Tuple<int[], int[], int[]>(avg, max, min);
+        }
+
+        public static void ExecuteQuery(string query)
+        {
+            var dbConnection = DBConnection.Instance();
+            if (dbConnection.IsConnect())
+            {
+                var command = new MySqlCommand(query, dbConnection.Connection);
+                command.ExecuteNonQuery();
+                //dbConnection.Close();
+            }
+        }
+
+    }
+
+    
+}
